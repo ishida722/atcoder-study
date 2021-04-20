65bacec1799887c423be2bf6a4a35708a7a71553
t-ishida <t-ishida@ditect.co.jp>
2021-04-20 11:57:07 +0900

    add

--- ABC197/b.py
+++ ABC197/b.py
@@ -1,7 +1,28 @@
+import numpy as np
 H, W, X, Y = map(int, input().split())
-S = [input().split() for _ in range(H)]
+S = [[0 if s=='.' else 1 for s in input()] for _ in range(H)]
 
 ans = 0
-for i in range(W):
-    w_count = 0
-    
+
+for x in range(X, 0, -1):
+    if S[x-1][Y-1] == 0:
+        ans += 1
+    else:
+        break
+for x in range(X, H):
+    if S[x-1][Y-1] == 0:
+        ans += 1
+    else:
+        break
+for y in range(Y, 0, -1):
+    if S[X-1][y-1] == 0:
+        ans += 1
+    else:
+        break
+for y in range(Y, W):
+    if S[X-1][y-1] == 0:
+        ans += 1
+    else:
+        break
+
+print(ans-3)
